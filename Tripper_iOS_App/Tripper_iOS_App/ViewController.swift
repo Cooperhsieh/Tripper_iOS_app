67cecd278450e604d3da3e4563bd12af494f1057
Cooperhsieh <cooper79216@gmail.com>
2020-10-23 19:33:18 +0800

    Open new iOS project

---
+++ Tripper_iOS_App/Tripper_iOS_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tripper_iOS_App
+//
+//  Created by Cooper on 2020/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
